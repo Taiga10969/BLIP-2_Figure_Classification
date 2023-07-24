90a7df9f50229bd0371b896bbef4ced8ade66c97
Taiga MASUDA <106218669+Taiga10969@users.noreply.github.com>
2023-07-24 17:21:46 +0900

    Create configs.py

---
+++ experiment/configs.py
@@ -0,0 +1,7 @@
+class Config(object):
+    def __init__(self):
+        
+        self.dataset_path = '/taiga/Datasets/moonshot-dataset'
+
+        self.category = ['chart', 'network architecture', 'qualitative evaluation']
+        self.template = 'this diagram type is {}.'
