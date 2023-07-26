f356fe5b4fbbc45ba632948566ddbcd745c3fb2e
Taiga MASUDA <106218669+Taiga10969@users.noreply.github.com>
2023-07-26 14:03:55 +0900

    Create utils.py

---
+++ experiment/utils.py
@@ -0,0 +1,10 @@
+import torch
+
+def setup_torch_seed(seed=1):
+    # pytorchに関連する乱数シードの固定を行う．
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+
+# 乱数シードを固定
+setup_torch_seed()
