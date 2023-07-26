3b250d465657c9fdabc01c4854dfa154c54f22a3
Taiga MASUDA <106218669+Taiga10969@users.noreply.github.com>
2023-07-26 14:03:32 +0900

    Create models.py

---
+++ experiment/models.py
@@ -0,0 +1,50 @@
+import configs
+import utils
+import datasets
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import transforms
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from collections import OrderedDict
+
+from torchinfo import summary
+
+from transformers import AutoProcessor, AutoTokenizer, Blip2ForConditionalGeneration
+from PIL import Image
+
+
+utils.setup_torch_seed()
+
+config = configs.Config()
+
+# check GPU usage
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+if torch.cuda.device_count()==0: print('Use 1 GPU')
+else: print(f'Use {torch.cuda.device_count()} GPUs')
+
+
+# model definition
+processor = AutoProcessor.from_pretrained(config.model_name)
+tokenizer = AutoTokenizer.from_pretrained(config.model_name)
+model = Blip2ForConditionalGeneration.from_pretrained(config.model_name, 
+                                                      torch_dtype=torch.float16)
+
+summary(model)
+
+for name, param in model.named_parameters():
+    print('name : ', name, end='\t\t')
+    print('param.requires_grad = ', param.requires_grad)
+
+
+# 'qformer.encoder'を含むサブモジュール以外のパラメータをフリーズする
+for name, param in model.named_parameters():
+    if 'qformer.encoder' not in name:
+        param.requires_grad = False
+
+
+for name, param in model.named_parameters():
+    print('name : ', name, end='\t\t')
+    print('param.requires_grad = ', param.requires_grad)
