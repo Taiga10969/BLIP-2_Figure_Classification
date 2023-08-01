4761e93091ddaaf09166db1880ee2913eac77fc4
Taiga MASUDA <106218669+Taiga10969@users.noreply.github.com>
2023-08-01 19:28:42 +0900

    Update main.py

--- experiment/main.py
+++ experiment/main.py
@@ -10,13 +10,17 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm
 from collections import OrderedDict
 
-from transformers import AutoProcessor, AutoTokenizer, Blip2ForConditionalGeneration
+from transformers import AutoProcessor, AutoTokenizer, Blip2ForConditionalGeneration, Blip2Model
 from PIL import Image
+
+import copy
 
 
 utils.setup_torch_seed()
 
 config = configs.Config()
+
+#pth = '/taiga/experiment/BLIP-2_figure_classification/record/train_best.pth'
 
 # check GPU usage
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -27,8 +31,8 @@ else: print(f'Use {torch.cuda.device_count()} GPUs')
 # model definition
 processor = AutoProcessor.from_pretrained(config.model_name)
 tokenizer = AutoTokenizer.from_pretrained(config.model_name)
-model = Blip2ForConditionalGeneration.from_pretrained(config.model_name, 
-                                                      torch_dtype=torch.float16)
+model = Blip2Model.from_pretrained(config.model_name, 
+                                    torch_dtype=torch.float16)
 
 ## 'qformer.encoder'を含むサブモジュール以外のパラメータをフリーズする
 
@@ -36,10 +40,16 @@ model = Blip2ForConditionalGeneration.from_pretrained(config.model_name,
 for name, param in model.named_parameters():
     if 'vision_model.encoder' in name:
         param.requires_grad = False
+# qformer.encoder をフリーズする
+for name, param in model.named_parameters():
+    if 'qformer.encoder' in name:
+        param.requires_grad = False
 
 #for name, param in model.named_parameters():
-#    if 'qformer.encoder' not in name:
+#    if any(f'language_model.model.decoder.layers.{layer_num}.' in name for layer_num in range(1, 16)):
 #        param.requires_grad = False
+
+#model.load_state_dict(torch.load(pth,map_location=torch.device(device)))
 
 model = nn.DataParallel(model)
 model.to(device)
@@ -72,8 +82,8 @@ test_dataset = datasets.ms_FigureClassification(dataset_path=config.dataset_path
                                                  )
 
 
-train_loader = DataLoader(train_dataset, batch_size=config.batch_size, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=config.batch_size, shuffle=False)
+train_loader = DataLoader(train_dataset, batch_size=config.batch_size, shuffle=True, num_workers=2)
+test_loader = DataLoader(test_dataset, batch_size=config.batch_size, shuffle=False, num_workers=2)
 
 optimizer = optim.AdamW(params = model.parameters(),
                         lr=config.lr, 
@@ -88,115 +98,112 @@ lr_scheduler = optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer=optimize
 best_val_loss=10.0
 best_train_loss=10.0
 iteration=0
+with torch.autocast(device_type='cuda'):
+    for epoch in range(1, config.epoch+1, 1):
+        with tqdm(train_loader) as pbar:
+            pbar.set_description(f'[train epoch : {epoch}]')
 
-for epoch in range(1, config.epoch+1, 1):
-    with tqdm(train_loader) as pbar:
-        pbar.set_description(f'[train epoch : {epoch}]')
+            model.train()
+            sum_train_loss = 0.0
+            train_loss = 0.0
+            train_count = 0
 
-        model.train()
-        sum_train_loss = 0.0
-        train_loss = 0.0
-        train_count = 0
+            for img_paths, prompts, labels in pbar:
+                train_count+=1
+                iteration+=1
+                optimizer.zero_grad()
 
-        for img_paths, prompts, labels in pbar:
-            train_count+=1
-            iteration+=1
-            optimizer.zero_grad()
-
-            image_list = []
-            for path in img_paths:
-                image = Image.open(path)
-                image_list.append(image)
+                image_list = []
+                for path in img_paths:
+                    image = Image.open(path)
+                    image_list.append(image)
 
 
-            inputs = processor(images=image_list, 
-                               text=prompts, 
-                               padding='max_length', 
-                               truncation=True, 
-                               max_length=config.max_length, 
-                               return_tensors='pt').to(device, torch.float16)
-            
-            labels = processor(text=labels, 
-                               padding='max_length', 
-                               truncation=True, 
-                               max_length=config.max_length, 
-                               return_tensors='pt')["input_ids"]
+                inputs = processor(images=image_list, 
+                                   text=prompts, 
+                                   padding='max_length', 
+                                   truncation=True, 
+                                   max_length=config.max_length, 
+                                   return_tensors='pt').to(device, torch.float16)
 
-            outputs = model(pixel_values=inputs["pixel_values"], 
-                            input_ids=inputs["input_ids"], 
-                            labels = labels)
+                labels = processor(text=labels, 
+                                   padding='max_length', 
+                                   truncation=True, 
+                                   max_length=config.max_length, 
+                                   return_tensors='pt')["input_ids"]
 
-            
-            loss = outputs.loss.mean()
+                outputs = model(pixel_values=inputs["pixel_values"], 
+                                input_ids=inputs["input_ids"], 
+                                labels = labels)
 
-            sum_train_loss += loss.item()
-            
-            loss.backward()
-            optimizer.step()
+
+                loss = outputs.loss.mean()
+                sum_train_loss += loss.item()
+
+                ## Check if the loss value is NaN
+                #if torch.isnan(loss):
+                #    print("Skipping iteration due to NaN loss.")
+                #    optimizer.zero_grad(set_to_none=True)
+                #    continue
+                #else:
+                #    sum_train_loss += loss.item()
+                #    loss.backward()
+                #    optimizer.step()
+
+                #if torch.isnan(loss):
+                #    # section 3
+                #    model = prev_model
+                #    optimizer = optim.AdamW(params = model.parameters(),
+                #                            lr=config.lr, 
+                #                            betas=config.betas, 
+                #                            eps=config.eps, 
+                #                            weight_decay=config.weight_decay)
+                #    optimizer.load_state_dict(prev_optimizer.state_dict())
+                #else:
+                #    prev_model = copy.deepcopy(model)
+                #    prev_optimizer = copy.deepcopy(optimizer)
+                #
+                #    optimizer.zero_grad()
+                #    loss.backward()
+                #    optimizer.step()
+
+
+                if torch.isnan(loss):
+                    # section 3
+                    model = prev_model
+                    optimizer = optim.AdamW(params = model.parameters(),
+                                            lr=config.lr, 
+                                            betas=config.betas, 
+                                            eps=config.eps, 
+                                            weight_decay=config.weight_decay)
+                    optimizer.load_state_dict(prev_optimizer.state_dict())
+                else:
+                    prev_model = model.module.cpu().clone().detach()
+                    prev_optimizer = torch.optim.Adam(prev_model.parameters())
+                    prev_optimizer.load_state_dict(optimizer.state_dict())
+
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+
+
+                #print('loss: ', loss)
+
+                #
+
+                #loss.backward()
+                #optimizer.step()
+                
+
+                ave_loss=sum_train_loss/train_count,
+
+                pbar.set_postfix(OrderedDict(loss=loss.item(), ave_loss=ave_loss[0], lr = optimizer.param_groups[0]['lr']))
+                with open(config.record_dir+'/loss_record.csv', 'a') as f:
+                    print(f'{iteration}, {loss.item()}', file=f)
+
+                if train_count%100 ==0 and best_train_loss > ave_loss[0]:
+                    best_train_loss = ave_loss[0]
+                    torch.save(model.module.state_dict(), config.record_dir+"/train_best.pth")
             lr_scheduler.step()
+            train_loss=sum_train_loss/train_count
 
-            ave_loss=sum_train_loss/train_count,
-
-            pbar.set_postfix(OrderedDict(loss=loss.item(), ave_loss=ave_loss[0], lr = optimizer.param_groups[0]['lr']))
-            with open(config.record_dir+'/loss_record.csv', 'a') as f:
-                print(f'{iteration}, {loss.item()}', file=f)
-            
-            if train_count%100 ==0 and best_train_loss > ave_loss[0]:
-                best_train_loss = ave_loss[0]
-                torch.save(model.module.state_dict(), config.record_dir+"/train_best.pth")
-            
-        train_loss=sum_train_loss/train_count
-
-        
-        
-
-    with tqdm(test_loader) as pbar:
-        pbar.set_description(f'[valid epoch : {epoch}]')
-
-        model.eval()
-        sum_valid_loss = 0.0
-        valid_loss = 0.0
-        valid_count = 0
-
-        for img_paths, prompts, labels in pbar:
-            valid_count+=1
-
-            image_list = []
-            for path in img_paths:
-                image = Image.open(path)
-                image_list.append(image)
-
-
-            inputs = processor(images=image_list, 
-                               text=prompts, 
-                               padding='max_length', 
-                               truncation=True, 
-                               max_length=config.max_length, 
-                               return_tensors='pt').to(device, torch.float16)
-            
-            labels = processor(text=labels, 
-                               padding='max_length', 
-                               truncation=True, 
-                               max_length=config.max_length, 
-                               return_tensors='pt')["input_ids"]
-
-            outputs = model(pixel_values=inputs["pixel_values"], 
-                            input_ids=inputs["input_ids"], 
-                            labels = labels)
-
-            
-            loss = outputs.loss.mean()
-            sum_valid_loss += loss.item()
-
-            pbar.set_postfix(OrderedDict(loss=loss.item(), ave_loss=sum_valid_loss/valid_count))
-
-        valid_loss=sum_valid_loss/valid_count
-        
-    torch.save(model.module.state_dict(), config.record_dir+"/epoch_"+str(epoch)+".pth")
-
-    if best_val_loss > valid_loss:
-        best_val_loss = valid_loss
-        torch.save(model.module.state_dict(), config.record_dir+"/best_val_loss.pth")
-
-    with open(config.record_dir+'/epoch_loss_record.csv', 'a') as f:
-        print(f'{epoch}, {train_loss}, {valid_loss}', file=f)
